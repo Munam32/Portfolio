2e189bc5425c8f327cb809982f8764ce1aa54682
munam <abdulmunam891@gmail.com>
2025-07-12 15:39:52 +0500

    Added Speed Insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable}>
+        <SpeedInsights />
         <Header />
         <StairTransition />
         <PageTransition>{children}</PageTransition>
